75f85c3a8e9f42c80088a382b00cd676fdb24acd
Vishal-8155 <bagsariyaa@gmail.com>
2023-10-13 18:34:37 +0530

    nav bar style updated

--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -11,16 +11,24 @@ export default function Nav() {
   }
   return (
     <div>
-      
-      {auth ?
-        <ul className='nav-ul'>
-          <li><Link to="/">Products</Link></li>
-          <li><Link to="/add">Add Products</Link></li>
-          <li><Link to="/update">Update Products</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})</Link></li>
 
-        </ul>
+      {auth ?
+        <nav class="navbar navbar-expand-lg ">
+        <div class="container-fluid">
+          <a class="navbar-brand text-light" href="#">Damas</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <li><Link to="/" class="nav-link text-light">Products</Link></li>
+              <li><Link to="/add" class="nav-link text-light">Add Products</Link></li>
+              <li><Link to="/profile" class="nav-link text-light">Profile</Link></li>
+              <li><Link onClick={logout} to="/signup" class="nav-link text-light">Logout ({JSON.parse(auth).name})</Link></li>
+            </div>
+          </div>
+        </div>
+      </nav>
         :
         <ul className='nav-ul nav-right'>
           <>
@@ -32,3 +40,4 @@ export default function Nav() {
     </div>
   )
 }
+
